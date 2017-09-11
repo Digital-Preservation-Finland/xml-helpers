de6eff73362defd3c60ae47b02f8243d3c3044f6
Juha Lehtonen <juha.lehtonen@csc.fi>
2017-09-11 19:10:39 +0300

    KDKPAS-1433 Tests added.

---
+++ tests/utils_test.py
@@ -0,0 +1,41 @@
+"""Test for XML utils"""
+
+import xml.etree.ElementTree as ET
+import common_xml_utils.utils as u
+
+def test_indent():
+    """test indent"""
+    xml = '<a><b/></a>'
+    ind_xml='<a>\n  <b />\n</a>\n'
+    tree = ET.fromstring(xml)
+    u.indent(tree)
+    assert ET.tostring(tree) == ind_xml
+
+
+def test_serialize():
+    """test serialize"""
+    ns = {'a': 'b'}
+    xml = '<a xmlns="b"><b/></a>'
+    ser_xml='<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n' \
+            '<a:a xmlns:a="b">\n  <a:b />\n</a:a>\n'
+    result = u.serialize(ET.fromstring(xml), ns)
+    assert result == ser_xml
+
+def test_register_namespaces():
+    """test register_namespaces"""
+    ns_map = {'a': 'b', 'c': 'd'}
+    u.register_namespaces(ns_map)
+
+    assert ET._namespace_map['b'] == 'a'
+    assert ET._namespace_map['d'] == 'c'
+
+def test_get_namespace():
+    """test get_namespace"""
+    ns_map = {'xsi': 'http://www.w3.org/2001/XMLSchema-instance'}
+    u.register_namespaces(ns_map)
+    elem = ET.Element(u.xsi_ns('a'))
+    assert u.get_namespace(elem) == '{http://www.w3.org/2001/XMLSchema-instance}'
+
+def test_xsi_ns():
+    """test xsi namespace"""
+    assert u.xsi_ns('a') == '{http://www.w3.org/2001/XMLSchema-instance}a'
