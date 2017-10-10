cdac311a3f02d226aadbdcdfd002ffcccc6bbc4f
Juha Lehtonen <juha.lehtonen@csc.fi>
2017-10-10 19:32:46 +0300

    KDKPAS-1433 Change xml.etree to lxml.etree

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,40 +1,24 @@
 """Test for XML utils"""
 
-import xml.etree.ElementTree as ET
+import lxml.etree as ET
 import xml_helpers.utils as u
-
-def test_indent():
-    """test indent"""
-    xml = '<a><b/></a>'
-    ind_xml='<a>\n  <b />\n</a>\n'
-    tree = ET.fromstring(xml)
-    u.indent(tree)
-    assert ET.tostring(tree) == ind_xml
 
 
 def test_serialize():
     """test serialize"""
     ns = {'a': 'b'}
-    xml = '<a xmlns="b"><b/></a>'
+    xml = '<a:x xmlns:a="b"><a:y/></a:x>'
     ser_xml='<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n' \
-            '<a:a xmlns:a="b">\n  <a:b />\n</a:a>\n'
-    result = u.serialize(ET.fromstring(xml), ns)
+            '<a:x xmlns:a="b">\n  <a:y/>\n</a:x>\n'
+    result = u.serialize(ET.fromstring(xml))
     assert result == ser_xml
 
-def test_register_namespaces():
-    """test register_namespaces"""
-    ns_map = {'a': 'b', 'c': 'd'}
-    u.register_namespaces(ns_map)
-
-    assert ET._namespace_map['b'] == 'a'
-    assert ET._namespace_map['d'] == 'c'
 
 def test_get_namespace():
     """test get_namespace"""
-    ns_map = {'xsi': 'http://www.w3.org/2001/XMLSchema-instance'}
-    u.register_namespaces(ns_map)
     elem = ET.Element(u.xsi_ns('a'))
     assert u.get_namespace(elem) == 'http://www.w3.org/2001/XMLSchema-instance'
+
 
 def test_xsi_ns():
     """test xsi namespace"""
--- xml_helpers/utils.py
+++ xml_helpers/utils.py
@@ -1,69 +1,30 @@
-"""Utility functions for handling XML data with xml.etree.ElementTree data
+"""Utility functions for handling XML data with lxml.etree data
 structures"""
 
-import re
-import xml.etree.ElementTree as ET
+import lxml.etree as ET
 
 
 XSI_NS = 'http://www.w3.org/2001/XMLSchema-instance'
 
 
-def indent(elem, level=0):
-    """Add indentation for the ElementTree elements
+def readfile(filename):
+    """Read file, remove blanks and comments"""
+    return ET.XMLParser(remove_blank_text=True, remove_comments=True)
 
-    Modifies the given element tree inplace.
-
-    :elem: Starting element
-
-    """
-    i = "\n" + level*"  "
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + "  "
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-        for elem in elem:
-            indent(elem, level+1)
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-    else:
-        if level and (not elem.tail or not elem.tail.strip()):
-            elem.tail = i
-
-
-def serialize(root_element, namespaces):
-    """Serialize ElementTree structure with PREMIS namespace mapping.
-
-    This modifies the default "ns0:tag" style prefixes to "premis:tag"
-    prefixes.
+def serialize(root_element):
+    """Serialize lxml.etree structure.
 
     :root_element: Starting element to serialize
-    :namespaces: Namespaces as dict "prefix:namespace"
     :returns: Serialized XML as string
 
     """
-    # We can't serialize an ElementTree, so serialize the tree's
-    # root Element instead
-    if isinstance(root_element, ET.ElementTree):
-        root_element = root_element.getroot()
-
-    register_namespaces(namespaces)
-    indent(root_element)
-
-    return ET.tostring(root_element, encoding='UTF-8')
-
-
-def register_namespaces(namespaces):
-    """Register given namespaces
-    """
-    for prefix, name in namespaces.items():
-        ET.register_namespace(prefix, name)
+    return ET.tostring(ET.cleanup_namespaces(root_element), pretty_print=True,
+                       xml_declaration=True, encoding='UTF-8')
 
 
 def get_namespace(elem):
     """return xml element's namespace"""
-    m = re.match('\{.*\}', elem.tag)
-    return m.group(0)[1:-1] if m else ''
+    return elem.nsmap[elem.prefix]
 
 
 def xsi_ns(tag):
