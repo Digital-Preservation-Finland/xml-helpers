fe2c8d1ecfccfe0ef7ef380c4f839a3dd35c4280
Thien Tran <thien.tran@csc.fi>
2019-03-11 14:53:23 +0000

    SAPA-33 Patch six-package to contain the version 1.12 tools

--- xml_helpers/utils.py
+++ xml_helpers/utils.py
@@ -107,3 +107,67 @@ def encode_utf8(text):
         return six.ensure_str(text)
     except TypeError:
         return text
+
+
+# pylint: disable=invalid-name, no-else-return
+# These pylint warnings were from the original six-developer.
+def _patch_six():
+    """Ensure the given six-package contains the mandatory
+    ensure_text- and ensure_str-functions.
+    """
+
+    def _ensure_text(s, encoding='utf-8', errors='strict'):
+        """Coerce *s* to six.text_type.
+
+        For Python 2:
+          - `unicode` -> `unicode`
+          - `str` -> `unicode`
+
+        For Python 3:
+          - `str` -> `str`
+          - `bytes` -> decoded to `str`
+
+        Direct copy from release 1.12::
+
+            https://github.com/benjaminp/six/blob/master/six.py#L892
+        """
+        if isinstance(s, six.binary_type):
+            return s.decode(encoding, errors)
+        elif isinstance(s, six.text_type):
+            return s
+        else:
+            raise TypeError("not expecting type '%s'" % type(s))
+
+    def _ensure_str(s, encoding='utf-8', errors='strict'):
+        """Coerce *s* to `str`.
+
+        For Python 2:
+          - `unicode` -> encoded to `str`
+          - `str` -> `str`
+
+        For Python 3:
+          - `str` -> `str`
+          - `bytes` -> decoded to `str`
+
+        Direct copy from release 1.12::
+
+            https://github.com/benjaminp/six/blob/master/six.py#L892
+        """
+        if not isinstance(s, (six.text_type, six.binary_type)):
+            raise TypeError("not expecting type '%s'" % type(s))
+        if six.PY2 and isinstance(s, six.text_type):
+            s = s.encode(encoding, errors)
+        elif six.PY3 and isinstance(s, six.binary_type):
+            s = s.decode(encoding, errors)
+        return s
+
+    six_funcs = {
+        'ensure_text': _ensure_text,
+        'ensure_str': _ensure_str
+    }
+    for attr_name, new_func in six_funcs.items():
+        if not hasattr(six, attr_name):
+            setattr(six, attr_name, new_func)
+
+
+_patch_six()
