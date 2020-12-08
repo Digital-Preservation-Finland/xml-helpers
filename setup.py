c4fbd5bd3e5ccae06cbf19f98a1f8d1c055aee3b
Thien Tran <thien.tran@csc.fi>
2020-12-08 17:01:31 +0200

    Revert "SAPA-526 Add __version__"
    This reverts commit 9cb548d717a0f955eccf525b2829bb830ec20103.
    
    Conflicts:
    	setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 """Install xml-helpers"""
 
-import re
 from setuptools import setup, find_packages
-
-with open('xml_helpers/__init__.py', 'r') as f:
-    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+from version import get_version
 
 
 def main():
@@ -13,7 +10,7 @@ def main():
         name='xml_helpers',
         packages=find_packages(exclude=['tests', 'tests.*']),
         include_package_data=True,
-        version=version,
+        version=get_version(),
         install_requires=['lxml', 'six']
     )
 
